dd774bddb1475cec7d6299ad95ebe3b8f7ed22f8
LittleDyingDuck <51399074+LittleDyingDuck@users.noreply.github.com>
2021-07-03 15:45:20 +1000

    Update setup.py

--- py2exe/setup.py
+++ py2exe/setup.py
@@ -1,7 +1,8 @@
 """
-Make sure you do pip install py2exe
+Make sure you do pip install py2exe, then when you're in the directory in command prompt type python <filename>.py py2exe
 
-This is to further prevent people from getting the webhook and whatever.
+This is to further prevent people from getting the webhook and whatever by using a python intepreter through a dll so no one actually needs python installed.
+
 
 """
 from distutils.core import setup
